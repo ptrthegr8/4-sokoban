04077780c7a3c898f85f3b76ccc5b84e87e356e3
Peter Marsh <p.marsh@mac.com>
2018-03-07 15:51:40 -0500

    lots of changes

--- main.js
+++ main.js
@@ -1,11 +1,21 @@
 let gameBoard = document.getElementById("gameBoard");
-var playerTop = 100;
-var playerLeft = 100;
-let boxLeft = 200;
+let playerDiv = document.getElementById("player");
+
 let posX = 2;
 let posY = 2;
-winningPos = 0;
-const map = [
+let tempVar = ""
+// const map = [
+//     "  WWWWW ",
+//     "WWW   W ",
+//     "WOSB  W ",
+//     "WWW BOW ",
+//     "WOWWB W ",
+//     "W W O WW",
+//     "WB XBBOW",
+//     "W   O  W",
+//     "WWWWWWWW"
+// ];
+var map = [
     "  WWWWW ",
     "WWW   W ",
     "WOSB  W ",
@@ -16,106 +26,180 @@ const map = [
     "W   O  W",
     "WWWWWWWW"
 ];
+//MAKES NESTED ARRAY
+let mapAltered = [];
+for (let i = 0; i < map.length; i++) {
+    mapAltered.push(map[i].split(""));
+}
+console.log(mapAltered);
+//LEGEND
 // "W" is a wall.
-// "S" is the player's starting position, and 
-// " " spaces are open floor. 
-//This map also uses three new characters:
+// "S" is the player's starting position and the player character.
+// " " spaces are open floor.
 // "O" is an initially empty storage location.
 // "B" is the starting position of a box/crate.
 // "X" is a storage location that starts with a box already on it.
-for (var r = 0; r < map.length; r++) {
-    var row = document.createElement("div");
-    row.classList.add("row");
-    for (var c = 0; c < map[r].length; c++) {
-        switch (map[r][c]) {
-            case "W":
-                var wall = document.createElement("div");
-                wall.classList.add("wall", "cell", "column");
-                row.appendChild(wall);
-                break;
-            case "O":
-                var finish = document.createElement("div");
-                finish.classList.add("finish", "cell", "column");
-                row.appendChild(finish);
-                break;
-            case "S":
-                var start = document.createElement("div");
-                start.classList.add("start", "cell", "column");
-                row.appendChild(start);
-                break;
-            case "X":
-                var alreadyBoxed = document.createElement("div");
-                alreadyBoxed.classList.add("cell", "column", "already-boxed");
-                row.appendChild(alreadyBoxed);
-                break;
-            case "B":
-                var box = document.createElement("div");
-                box.classList.add("cell", "column", "box-class");
-                row.appendChild(box);
-                break;
-            case " ":
-                var empty = document.createElement("div");
-                empty.classList.add("cell", "column");
-                row.appendChild(empty);
-
+//DRAWS BOARD
+function drawBoard() {
+    document.getElementById("gameBoard").innerHTML = "";
+    for (let r = 0; r < mapAltered.length; r++) {
+        let row = document.createElement("div");
+        row.classList.add("row");
+        for (let c = 0; c < mapAltered[r].length; c++) {
+            switch (mapAltered[r][c]) {
+                case "W":
+                    let wall = document.createElement("div");
+                    wall.classList.add("wall", "cell", "column");
+                    row.appendChild(wall);
+                    break;
+                case "O":
+                    let finish = document.createElement("div");
+                    finish.classList.add("finish", "cell", "column");
+                    row.appendChild(finish);
+                    break;
+                case "S":
+                    let playerDiv = document.createElement("div");
+                    playerDiv.classList.add("cell", "column");
+                    playerDiv.setAttribute("id", "player");
+                    row.appendChild(playerDiv);
+                    break;
+                case "X":
+                    let alreadyBoxed = document.createElement("div");
+                    alreadyBoxed.classList.add("cell", "column", "already-boxed");
+                    row.appendChild(alreadyBoxed);
+                    break;
+                case "B":
+                    let box = document.createElement("div");
+                    box.classList.add("cell", "column", "box-class");
+                    row.appendChild(box);
+                    break;
+                case " ":
+                    let empty = document.createElement("div");
+                    empty.classList.add("cell", "column");
+                    row.appendChild(empty);
+            }
         }
+        gameBoard.appendChild(row);
     }
-    gameBoard.appendChild(row);
 }
+//
 
-
-'use strict';
-
+drawBoard()
+//KEYBOARD LOGIC 
 document.addEventListener('keydown', (event) => {
     const keyName = event.key;
-    console.log('keydown event\n\n' + 'key: ' + keyName);
-    if (keyName === "ArrowDown") {
-        if (posY < 9 && posY > 0) {
-            if (map[posY + 1][posX] !== "W") {
-                playerTop += 50;
-                posY += 1;
-                document.getElementById("player").style.top = playerTop + "px";
-            }
-        }
-    }
-    //
-    if (keyName === "ArrowUp") {
-        if (posY < 9 && posY > 0) {
-            if (map[posY - 1][posX] !== "W" ) {
-                playerTop -= 50;
-                posY -= 1;
-                document.getElementById("player").style.top = playerTop + "px";
-            }
-        }
-
-    }
-    //
+    console.log(mapAltered);
+    console.log('key: ' + keyName);
+    // RIGHT ARROW
     if (keyName === "ArrowRight") {
-        if (posX < 8 && posX >= 0) {
-            if (map[posY][posX + 1] !== "W") {
-                posX += 1;
-                playerLeft += 50;
-                document.getElementById("player").style.left = playerLeft + "px";
-                // if (map[posY][posX + 1] === "B") {
-                //     boxLeft += 50;
-                //     document.getElementsByClassName("box-class").style.left = boxLeft + "px";
-                //     console.log(boxLeft);
-                // }
+        if (mapAltered[posY][posX] === "S") {
+            // moves box if next space is empty
+            if (mapAltered[posY][posX + 1] === "B" && mapAltered[posY][posX + 2] === " ") {
+                mapAltered[posY][posX] = " ";
+                mapAltered[posY][posX + 1] = "S";
+                mapAltered[posY][posX + 2] = "B";
+                posX++;
+                drawBoard();
+            } else if (mapAltered[posY][posX + 1] === " ") {
+                //moves player to empty space
+                mapAltered[posY][posX] = " ";
+                mapAltered[posY][posX + 1] = "S";
+                posX++;
+                drawBoard();
+            } else if (mapAltered[posY][posX + 1] === "B" && mapAltered[posY][posX + 2] === "O") {
+                //moves box to empty storage location
+                mapAltered[posY][posX] = " ";
+                mapAltered[posY][posX + 1] = "S"
+                mapAltered[posY][posX + 2] = "X";
+                posX++;
+                drawBoard();
             }
         }
     }
-    //
+    // LEFT ARROW
     if (keyName === "ArrowLeft") {
-        if (posX < 8 && posX >= 0) {
-            if (map[posY][posX - 1] !== "W") {
-                posX -= 1;
-                playerLeft -= 50;
-                document.getElementById("player").style.left = playerLeft + "px";
+        if (mapAltered[posY][posX] === "S") {
+            //moves player to empty space
+            if (mapAltered[posY][posX - 1] === " ") {
+                mapAltered[posY][posX] = " ";
+                mapAltered[posY][posX - 1] = "S";
+                posX--;
+                drawBoard();
+            } else if (mapAltered[posY][posX - 1] === "B" && mapAltered[posY][posX - 2] === " ") {
+                //moves box if next cell is empty
+                mapAltered[posY][posX] = " ";
+                mapAltered[posY][posX - 1] = "S";
+                mapAltered[posY][posX - 2] = "B";
+                posX--;
+                drawBoard();
+            } else if (mapAltered[posY][posX - 1] === "B" && mapAltered[posY][posX - 2] === "O") {
+                //moves box to empty storage location
+                mapAltered[posY][posX] = " ";
+                mapAltered[posY][posX - 1] = "S"
+                mapAltered[posY][posX - 2] = "X";
+                posX--;
+                drawBoard();
+
+            }
+        }
+    }
+    // DOWN ARROW
+    if (keyName === "ArrowDown") {
+        if (mapAltered[posY][posX] === "S") {
+            // moves player down to empty space
+            if (mapAltered[posY + 1][posX] === " ") {
+                mapAltered[posY + 1][posX] = "S";
+                mapAltered[posY][posX] = " ";
+                posY++;
+                drawBoard();
+            } else if (mapAltered[posY + 1][posX] === "O") { //moves player to empty storage location ***
+                tempVar = "O";
+                mapAltered[posY][posX] = " ";
+                mapAltered[posY + 1][posX] = "S";
+                posY++;
+                drawBoard();
+            } else if (mapAltered[posY + 1][posX] === "B" && mapAltered[posY + 2][posX] === " ") {
+                //moves box if next cell is empty
+                mapAltered[posY][posX] = " ";
+                mapAltered[posY + 1][posX] = "S";
+                mapAltered[posY + 2][posX] = "B";
+                posY++;
+                drawBoard();
+            } else if (mapAltered[posY + 1][posX] === "B" && mapAltered[posY + 2][posX] === "O") {
+                //moves player to empty storage location
+                mapAltered[posY][posX] = " ";
+                mapAltered[posY + 1][posX] = "S";
+                mapAltered[posY + 2][posX] = "X";
+                posY++;
+                drawBoard();
+            }
+        }
+    }
+    // UP ARROW
+    if (keyName === "ArrowUp") {
+        if (mapAltered[posY][posX] === "S") {
+            //moves player to empty space
+            if (mapAltered[posY - 1][posX] === " ") {
+                mapAltered[posY - 1][posX] = "S";
+                mapAltered[posY][posX] = " ";
+                posY--;
+                drawBoard();
+            } else if (mapAltered[posY - 1][posX] === "B" && mapAltered[posY - 2][posX] === " ") {
+                //moves box if next cell is empty
+                mapAltered[posY][posX] = " ";
+                mapAltered[posY - 1][posX] = "S";
+                mapAltered[posY - 2][posX] = "B";
+                posY--;
+                drawBoard();
+            } else if (mapAltered[posY - 1][posX] === "B" && mapAltered[posY - 2][posX] === "O") {
+                //moves player to empty storage location
+                mapAltered[posY][posX] = " ";
+                mapAltered[posY - 1][posX] = "S";
+                mapAltered[posY - 2][posX] = "X";
+                posY--;
+                drawBoard();
             }
         }
     }
     //
-    if (winningPos === 7) {
-        alert("You win!");
-    }
 });
